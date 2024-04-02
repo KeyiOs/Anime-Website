42cb52fed2375e5d44a13529c2b36dfd6fdc687b
KeyiOs <77984692+KeyiOs@users.noreply.github.com>
2024-04-02 19:31:48 +0200

    Navigation Menu [Initial]

--- JS/macros.js
+++ JS/macros.js
@@ -4,3 +4,23 @@ function switchToPage(pageName, delay) {
         window.location.href = pageURL;
     }, delay*1000);
 }
+
+function applyMargin() {
+    var lis = document.querySelectorAll(`li`);
+    lis.forEach(function(li, index) {
+        setTimeout(function() {
+            var wrapper = li.querySelector('.wrapper');
+            wrapper.style.transition = `margin-left 0.4s ease`;
+            wrapper.style.marginLeft = 8 + 'px';
+
+            setTimeout(function() {
+                wrapper.style.transition = `margin-left 0.4s ease`;
+                wrapper.style.marginLeft = '0px';
+            }, 400);
+        }, index * 120);
+    });
+}
+
+function navigationWave() {
+    setInterval(applyMargin, 5000);
+}
